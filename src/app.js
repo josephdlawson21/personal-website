0e1c4495f424bf1ee7e2b3ab8bac8bdfa0c2c204
josephdlawson21 <josephdlawson21@gmail.com>
2018-04-17 16:05:21 -0400

    added engine

--- src/app.js
+++ src/app.js
@@ -4,14 +4,11 @@ import Resume from './components/resume'
 import Blogs from './components/blogs'
 
 class App extends Component {
-
-
   componentDidMount() {
     fetch('https://medium.com/@josephdlawson21/latest?format=json')
       .then(res => res.json())
       .then(console.log)
   }
-
 
   render() {
     return (
