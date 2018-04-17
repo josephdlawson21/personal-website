4994247015b48cbba30f286d5923b2bc095c8d35
josephdlawson21 <josephdlawson21@gmail.com>
2018-04-17 15:51:05 -0400

    fetch

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ import Resume from './components/resume'
 import Blogs from './components/blogs'
 
 class App extends Component {
+
+
+  componentDidMount() {
+    fetch('https://medium.com/@josephdlawson21/latest?format=json')
+      .then(res => res.json())
+      .then(console.log)
+  }
+
+
   render() {
     return (
       <div className="App">
